838d5f3ffa50c5e63630c2189f2511184ecd4a10
lukebaggett <lukebaggett.mail@gmail.com>
2014-09-27 23:43:51 -0400

    listen and connect moved to functions to save space

--- powercat.ps1
+++ powercat.ps1
@@ -34,7 +34,71 @@ function powercat
   }
 
   [console]::TreatControlCAsInput=$True
-  # Move if($l) to listener function to shorten script
+
+  function Listen
+  {
+    param($p,$t)
+  
+    Write-Verbose ("Listening on [0.0.0.0] (port " + $p + ")")
+    $Socket = New-Object System.Net.Sockets.TcpListener $p
+    $Socket.Start()
+    $AcceptHandle = $Socket.BeginAcceptTcpClient($null, $null)
+    $Stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
+    while($True)
+    {
+      if($Host.UI.RawUI.KeyAvailable)
+      {
+        Read-Host
+      }
+      if($Stopwatch.Elapsed.TotalSeconds -gt $t)
+      {
+        $Socket.Stop()
+        $Stopwatch.Stop()
+        return 1
+      }
+      if($AcceptHandle.IsCompleted)
+      {
+        $Client = $Socket.EndAcceptTcpClient($AcceptHandle)
+        break
+      }
+    }
+    $Stopwatch.Stop()
+    Write-Verbose ("Connection from [" + $Client.Client.RemoteEndPoint.Address.IPAddressToString + "] port " + $port + " [tcp] accepted (source port " + $Client.Client.RemoteEndPoint.Port + ")")
+    $Stream = $Client.GetStream()
+    return $Stream
+  }
+  
+  function Connect
+  {
+    param($c,$p,$t)
+    Write-Verbose "Connecting..."
+    $Socket = New-Object System.Net.Sockets.TcpClient
+    $ConnectHandle = $Socket.BeginConnect($c,$p,$null,$null)
+    $Stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
+    while($True)
+    {
+      if($Host.UI.RawUI.KeyAvailable)
+      {
+        Read-Host
+      }
+      if($Stopwatch.Elapsed.TotalSeconds -gt $t)
+      {
+        $Socket.Close()
+        $Stopwatch.Stop()
+        return 1
+      }
+      if($ConnectHandle.IsCompleted)
+      {
+        $Socket.EndConnect($ConnectHandle)
+        break
+      }
+    }
+    $Stopwatch.Stop()
+    if($Socket -eq $null){return 2}
+    Write-Verbose ("Connection to " + $c + ":" + $p + " [tcp] succeeeded!")
+    $Stream = $Socket.GetStream()
+    return $Stream
+  }
   
   if($e -eq "")
   {
@@ -42,66 +106,19 @@ function powercat
     {
       if($l)
       {
-        Write-Verbose ("Listening on [0.0.0.0] (port " + $p + ")")
-        $Socket = New-Object System.Net.Sockets.TcpListener $p
-        $Socket.Start()
-        $AcceptHandle = $Socket.BeginAcceptTcpClient($null, $null)
-        $Stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
-        while($True)
-        {
-          if($Host.UI.RawUI.KeyAvailable)
-          {
-            Read-Host
-          }
-          if($Stopwatch.Elapsed.TotalSeconds -gt $t)
-          {
-            Write-Output "Listener Timeout."
-            $Break=$True
-            break
-          }
-          if($AcceptHandle.IsCompleted)
-          {
-            $Client = $Socket.EndAcceptTcpClient($AcceptHandle)
-            break
-          }
-        }
-        if($Break){break}
-        $Stopwatch.Stop()
-        Write-Verbose ("Connection from [" + $Client.Client.RemoteEndPoint.Address.IPAddressToString + "] port " + $port + " [tcp] accepted (source port " + $Client.Client.RemoteEndPoint.Port + ")")
-        $Stream = $Client.GetStream()
+        $Stream = Listen $p $t
       }
       else
       {
-        Write-Verbose "Connecting..."
-        $Socket = New-Object System.Net.Sockets.TcpClient
-        $ConnectHandle = $Socket.BeginConnect($c,$p,$null,$null)
-        $Stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
-        while($True)
-        {
-          if($Host.UI.RawUI.KeyAvailable)
-          {
-            Read-Host
-          }
-          if($Stopwatch.Elapsed.TotalSeconds -gt $t)
-          {
-            Write-Output "Connection Timeout."
-            $Break=$True
-            break
-          }
-          if($ConnectHandle.IsCompleted)
-          {
-            $Socket.EndConnect($ConnectHandle)
-            break
-          }
-        }
-        if($Socket -eq $null){Write-Error 'Unable To Connect'; return}
-
-        Write-Verbose ("Connection to " + $c + ":" + $p + " [tcp] succeeeded!")
-        $Stream = $Socket.GetStream()
+        $Stream = Connect $c $p $t
       }
+
+      if($Stream -eq 1){return "Timeout."}
+      if($Stream -eq 2){return "Connection Error."}
 
       $Buffer = New-Object System.Byte[] 1
       $Encoding = New-Object System.Text.AsciiEncoding
+
       $StreamDestinationBuffer = New-Object System.Byte[] 1
       $StreamReadOperation = $Stream.BeginRead($StreamDestinationBuffer, 0, 1, $null, $null)
       $ToStreamString = ""
@@ -148,64 +165,16 @@ function powercat
     {
       if($l)
       {
-        Write-Verbose ("Listening on [0.0.0.0] (port " + $p + ")")
-        $Socket = New-Object System.Net.Sockets.TcpListener $p
-        $Socket.Start()
-        $AcceptHandle = $Socket.BeginAcceptTcpClient($null, $null)
-        $Stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
-        while($True)
-        {
-          if($Host.UI.RawUI.KeyAvailable)
-          {
-            Read-Host
-          }
-          if($Stopwatch.Elapsed.TotalSeconds -gt $t)
-          {
-            Write-Output "Listener Timeout."
-            $Break=$True
-            break
-          }
-          if($AcceptHandle.IsCompleted)
-          {
-            $Client = $Socket.EndAcceptTcpClient($AcceptHandle)
-            break
-          }
-        }
-        if($Break){break}
-        $Stopwatch.Stop()
-        Write-Verbose ("Connection from [" + $Client.Client.RemoteEndPoint.Address.IPAddressToString + "] port " + $port + " [tcp] accepted (source port " + $Client.Client.RemoteEndPoint.Port + ")")
-        $Stream = $Client.GetStream()
+        $Stream = Listen $p $t
       }
       else
       {
-        Write-Verbose "Connecting..."
-        $Socket = New-Object System.Net.Sockets.TcpClient
-        $ConnectHandle = $Socket.BeginConnect($c,$p,$null,$null)
-        $Stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
-        while($True)
-        {
-          if($Host.UI.RawUI.KeyAvailable)
-          {
-            Read-Host
-          }
-          if($Stopwatch.Elapsed.TotalSeconds -gt $t)
-          {
-            Write-Output "Connection Timeout."
-            $Break=$True
-            break
-          }
-          if($ConnectHandle.IsCompleted)
-          {
-            $Socket.EndConnect($ConnectHandle)
-            break
-          }
-        }
-        if($Socket -eq $null){Write-Error 'Unable To Connect'; return}
-
-        Write-Verbose ("Connection to " + $c + ":" + $p + " [tcp] succeeeded!")
-        $Stream = $Socket.GetStream()
+        $Stream = Connect $c $p $t
       }
-      
+
+      if($Stream -eq 1){return "Timeout."}
+      if($Stream -eq 2){return "Connection Error."}
+
       $Buffer = New-Object System.Byte[] 1
       $Encoding = New-Object System.Text.AsciiEncoding
 
